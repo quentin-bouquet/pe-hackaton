4e68c9d36e267d30860785495943b377c78423b6
Valentin Deumier <valentin.deumier@etu.minesparis.psl.eu>
2023-10-31 15:10:28 +0100

    Affichage des highs et des lows

--- Affichage_Données.py
+++ Affichage_Données.py
@@ -3,11 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+plt.style.use('seaborn-v0_8')
+sns.set(rc={'figure.figsize': (8, 4)})
+
 df = pd.read_csv('Apple.csv')
 
 df['Date'] = pd.to_datetime(df['Date'], format = '%m/%d/%Y')
 
 df.set_index('Date', inplace = True)
-df.sort_index()
+df.sort_index(inplace = True)
 
-print(df.head(5))
+df['High'] = df['High'].apply(lambda x: float(x[1:]))
+df['Low'] = df['Low'].apply(lambda x: float(x[1:]))
+df['High'].plot(c='b')
+df['Low'].plot(c='r')
+
+plt.show()
