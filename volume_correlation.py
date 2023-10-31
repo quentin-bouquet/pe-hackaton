f95836ecd38f85b7d787727be3dee8f6c7c881f7
Taha Oulhazzan <taha.oulhazzan@gmail.com>
2023-10-31 15:42:50 +0100

    comit correlation

--- volume_correlation.py
+++ volume_correlation.py
@@ -5,8 +5,14 @@ amazon = pd.read_csv('Amazon.csv')
 netflix = pd.read_csv('Netflix.csv')
 meta = pd.read_csv('Meta.csv')
 apple = pd.read_csv('Apple.csv')
+cisco = pd.read_csv('Amazon.csv')
+microsoft = pd.read_csv('Amazon.csv')
+qualcomm = pd.read_csv('Amazon.csv')
+starbucks = pd.read_csv('Amazon.csv')
+amd = pd.read_csv('Amazon.csv')
+tesla = pd.read_csv('Amazon.csv')
 
-apple.plot()
+
 
 
 
