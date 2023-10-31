fb3dc8a96d35e9a5c550641cd9eb7c7b67990d23
Valentin Deumier <valentin.deumier@etu.minesparis.psl.eu>
2023-10-31 16:58:16 +0100

    Ajouts des PNG d'actions

--- Affichage_Données.py
+++ Affichage_Données.py
@@ -6,7 +6,7 @@ import seaborn as sns
 plt.style.use('seaborn-v0_8')
 sns.set(rc={'figure.figsize': (8, 4)})
 
-df = pd.read_csv('Apple.csv')
+df = pd.read_csv('Meta.csv')
 
 df['Date'] = pd.to_datetime(df['Date'], format = '%m/%d/%Y')
 
@@ -17,6 +17,6 @@ df['High'] = df['High'].apply(lambda x: float(x[1:]))
 df['Low'] = df['Low'].apply(lambda x: float(x[1:]))
 df['High'].resample('W').mean().plot(c='b')
 df['Low'].resample('W').mean().plot(c='r')
-plt.title("Evolution du prix des actions d'Apple (b=high, r=low)")
+plt.title("Evolution du prix des actions de Meta (b=high, r=low)")
 
 plt.show()
