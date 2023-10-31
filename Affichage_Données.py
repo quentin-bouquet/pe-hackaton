52aeed5dd5f0fa37090d9ff2389bb9bb58e1d7cd
Valentin Deumier <valentin.deumier@etu.minesparis.psl.eu>
2023-10-31 14:49:52 +0100

    Parser les dates

--- Affichage_Données.py
+++ Affichage_Données.py
@@ -1,7 +1,13 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv('Apple.csv')
+
+df['Date'] = pd.to_datetime(df['Date'], format = '%m/%d/%Y')
+
+df.set_index('Date', inplace = True)
+df.sort_index()
 
 print(df.head(5))
